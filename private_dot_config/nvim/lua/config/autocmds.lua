e544ee10eb26d428f597300c97b72329cfa6066c
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-03-19 11:44:09 +0000

    feat(nvim): setup chezmoi integration

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -12,29 +12,3 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.api.nvim_del_augroup_by_name('lazyvim_json_conceal')
-
---- Automatically applies chezmoi on quit
-local chezmoi_dir = vim.fn.expand('~/.local/share/chezmoi')
-local chezmoi_active = false
-
--- set flag when entering before saving
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = chezmoi_dir .. '/*',
-  callback = function()
-    chezmoi_active = true
-  end,
-})
-
-vim.api.nvim_create_user_command('ChezmoiApply', function()
-  vim.fn.system('chezmoi apply')
-  -- we don't need to do that again if we already applied chezmoi
-  chezmoi_active = false
-end, {})
-
--- check the flag on VimLeave and run `chezmoi apply` if true
-vim.api.nvim_create_autocmd('VimLeave', {
-  pattern = '*',
-  callback = function()
-    if chezmoi_active then vim.cmd('ChezmoiApply') end
-  end,
-})
