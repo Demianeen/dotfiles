494534dfb9364c561dec32857d068b6338ce0f78
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-03-19 20:44:31 +0000

    feat(nvim): update neo-tree settings; setup lsp-file-operations

--- private_dot_config/nvim/lua/plugins/ui/neo-tree.lua
+++ private_dot_config/nvim/lua/plugins/ui/neo-tree.lua
@@ -1,21 +1,116 @@
+local node_close_or_goto_parent = function(state)
+  local node = state.tree:get_node()
+
+  if
+    (node.type == 'directory' or node:has_children()) and node:is_expanded()
+  then
+    state.commands.toggle_node(state)
+  else
+    require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+  end
+end
+
+local node_open_or_file_open = function(state)
+  local node = state.tree:get_node()
+
+  if node.type == 'directory' or node:has_children() then
+    if not node:is_expanded() then
+      state.commands.toggle_node(state)
+    else
+      require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+    end
+  else
+    state.commands.open(state)
+  end
+end
+
+local copy_selector = function(state)
+  local node = state.tree:get_node()
+  local filepath = node:get_id()
+  local filename = node.name
+  local messages = { { '\nChoose to copy to clipboard:\n', 'Normal' } }
+  local modify = vim.fn.fnamemodify
+
+  local results = {
+    e = { val = modify(filename, ':e'), msg = 'Extension only' },
+    f = { val = filename, msg = 'Filename' },
+    F = { val = modify(filename, ':r'), msg = 'Filename w/o extension' },
+    h = { val = modify(filepath, ':~'), msg = 'Path relative to Home' },
+    c = { val = modify(filepath, ':.'), msg = 'Path relative to CWD' },
+    a = { val = filepath, msg = 'Absolute path' },
+  }
+
+  for i, result in pairs(results) do
+    if result.val and result.val ~= '' then
+      vim.list_extend(messages, {
+        { ('%s.'):format(i), 'Identifier' },
+        { (' %s: '):format(result.msg) },
+        { result.val, 'String' },
+        { '\n' },
+      })
+    end
+  end
+
+  vim.api.nvim_echo(messages, false, {})
+
+  local result = results[vim.fn.getcharstr()]
+
+  if result and result.val and result.val ~= '' then
+    vim.notify('Copied: ' .. result.val)
+    vim.fn.setreg('+', result.val)
+  end
+end
+
+local system_open = function(state)
+  local node = state.tree:get_node()
+  local path = node:get_id()
+
+  vim.fn.jobstart({ 'open', path }, { detach = true })
+end
+
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  opts = {
-    event_handlers = {
-      {
-        -- event = 'neo_tree_buffer_leave', -- then on each vtsls
-        -- reinitialization the neo tree is closed
-        event = 'file_opened',
-        handler = function(_)
-          vim.cmd('Neotree close')
-        end,
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    opts = {
+      show_scrolled_off_parent_node = true,
+      use_popups_for_input = true,
+      popup_border_style = 'rounded',
+
+      event_handlers = {
+        {
+          -- event = 'neo_tree_buffer_leave', -- then on each vtsls
+          -- reinitialization the neo tree is closed
+          event = 'file_opened',
+          handler = function(_)
+            vim.cmd('Neotree close')
+          end,
+        },
+        {
+          event = 'neo_tree_popup_input_ready',
+          handler = function(state)
+            vim.notify(vim.inspect(state))
+            vim.cmd('stopinsert')
+          end,
+        },
       },
-    },
-    filesystem = {
+      commands = {
+        node_open_or_file_open = node_open_or_file_open,
+        node_close_or_goto_parent = node_close_or_goto_parent,
+        copy_selector = copy_selector,
+        system_open = system_open,
+      },
+      filesystem = {
+        filtered_items = {
+          never_show = { '.DS_Store', '.git' },
+        },
+      },
       window = {
         mappings = {
-          ['n'] = 'add',
-          ['N'] = 'add_directory',
+          ['<space>'] = false,
+          ['h'] = 'node_close_or_goto_parent',
+          ['l'] = 'node_open_or_file_open',
+          ['Y'] = 'copy_selector',
+          ['O'] = 'system_open',
           -- fzf content of files in the dir under the cursor
           ['g'] = function(state)
             -- get the current node
@@ -42,53 +137,19 @@ return {
               ),
             })
           end,
-          ['c'] = {},
-          ['cf'] = {
-            'copy',
-            config = {
-              show_path = 'relative', -- "none", "relative", "absolute"
-            },
-          },
-          ['cp'] = function(state)
-            local current_file_path = state.tree:get_node().path
-            vim.fn.setreg('+', current_file_path)
-          end,
-          ['cn'] = function(state)
-            local current_file_name = state.tree:get_node().name
-            vim.fn.setreg('+', current_file_name)
-          end,
-          ['h'] = function(state)
-            local node = state.tree:get_node()
-            -- if node.type == 'directory' and node:is_expanded() then
-            --   require('neo-tree.sources.filesystem').toggle_directory(
-            --     state,
-            --     node
-            --   )
-            -- else
-            require('neo-tree.ui.renderer').focus_node(
-              state,
-              node:get_parent_id()
-            )
-            -- end
-          end,
-          ['l'] = function(state)
-            local node = state.tree:get_node()
-            if node.type == 'directory' then
-              if not node:is_expanded() then
-                require('neo-tree.sources.filesystem').toggle_directory(
-                  state,
-                  node
-                )
-              elseif node:has_children() then
-                require('neo-tree.ui.renderer').focus_node(
-                  state,
-                  node:get_child_ids()[1]
-                )
-              end
-            end
-          end,
         },
       },
+    },
+  },
+  {
+    {
+      'antosha417/nvim-lsp-file-operations',
+      optional = true,
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-neo-tree/neo-tree.nvim',
+      },
+      opts = {},
     },
   },
 }
