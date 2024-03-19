3627a6b215483eaeb5594ca0e90ec69274987873
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-03-19 14:46:19 +0000

    refactor(nvim): rename vtsls to lsp

--- private_dot_config/nvim/lua/plugins/lang/typescript/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lang/typescript/lsp.lua
@@ -3,7 +3,10 @@ return {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       if type(opts.ensure_installed) == 'table' then
-        vim.list_extend(opts.ensure_installed, { 'typescript', 'tsx' })
+        vim.list_extend(
+          opts.ensure_installed,
+          { 'typescript', 'tsx', 'embedded_template' }
+        )
       end
     end,
   },
