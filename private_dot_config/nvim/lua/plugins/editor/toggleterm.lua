10f90b2583afae5d151fc3e0e0f33916937eb974
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-03-25 11:15:57 +0000

    feat(nvim): return quit term keymap; add clear keymap

--- private_dot_config/nvim/lua/plugins/editor/toggleterm.lua
+++ private_dot_config/nvim/lua/plugins/editor/toggleterm.lua
@@ -5,6 +5,7 @@ vim.api.nvim_create_autocmd('FileType', {
 
 return {
   'akinsho/toggleterm.nvim',
+  event = 'VeryLazy',
   opts = {
     size = function(term)
       if term.direction == 'horizontal' then
@@ -21,4 +22,45 @@ return {
     --   end,
     -- },
   },
+  config = function(_, opts)
+    require('toggleterm').setup(opts)
+
+    vim.api.nvim_create_autocmd('TermOpen', {
+      pattern = 'term://*toggleterm#*',
+      callback = function()
+        local keymap_ops = require('util.keymaps')
+
+        -- del buffer
+        vim.api.nvim_buf_set_keymap(
+          0,
+          't',
+          '<C-q>',
+          '<Cmd>lua require("mini.bufremove").delete(0)<CR>',
+          keymap_ops('Terminate terminal')
+        )
+
+        -- clear terminal
+        vim.api.nvim_buf_create_user_command(0, 'ClearTerminal', function()
+          local term = require('toggleterm.terminal')
+          local termid = term.get_focused_id()
+
+          if termid then
+            local current_terminal = term.get(termid)
+            if current_terminal then
+              current_terminal:clear()
+            else
+              print('No terminal found with this ID.')
+            end
+          end
+        end, { desc = 'Clear the current ToggleTerm terminal' })
+        vim.api.nvim_buf_set_keymap(
+          0,
+          't',
+          '<C-l>',
+          '<CMD>ClearTerminal<CR>',
+          keymap_ops('Clear terminal')
+        )
+      end,
+    })
+  end,
 }
