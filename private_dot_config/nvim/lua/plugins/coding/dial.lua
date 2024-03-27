293fd011fb6c211677329bbc52dbee0aec997708
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-03-27 06:11:54 +0000

    feat(nvim): add dial python bool; adapt to lazy extra

--- private_dot_config/nvim/lua/plugins/coding/dial.lua
+++ private_dot_config/nvim/lua/plugins/coding/dial.lua
@@ -1,12 +1,6 @@
 return {
   'monaqa/dial.nvim',
-  keys = {
-    { '<C-a>', '<Plug>(dial-increment)', mode = { 'n', 'v' } },
-    { '<C-x>', '<Plug>(dial-decrement)', mode = { 'n', 'v' } },
-    { 'g<C-a>', 'g<Plug>(dial-increment)', mode = { 'n', 'v' }, remap = true },
-    { 'g<C-x>', 'g<Plug>(dial-decrement)', mode = { 'n', 'v' }, remap = true },
-  },
-  config = function()
+  opts = function()
     local augend = require('dial.augend')
 
     local logical_alias = augend.constant.new({
@@ -15,7 +9,9 @@ return {
       cyclic = true,
     })
 
-    local ordinalNumbers = augend.constant.new({
+    local ordinal_numbers = augend.constant.new({
+      -- elements through which we cycle. When we increment, we go down
+      -- On decrement we go up
       elements = {
         'first',
         'second',
@@ -28,7 +24,10 @@ return {
         'ninth',
         'tenth',
       },
+      -- if true, it only matches strings with word boundary. firstDate wouldn't work for example
       word = false,
+      -- do we cycle back and forth (tenth to first on increment, first to tenth on decrement).
+      -- Otherwise nothing will happen when there are no further values
       cyclic = true,
     })
 
@@ -65,121 +64,84 @@ return {
       cyclic = true,
     })
 
-    require('dial.config').augends:register_group({
-      default = {
-        augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
-        augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-        augend.date.alias['%Y/%m/%d'], -- date (2022/02/19, etc.)
+    local capitalized_boolean = augend.constant.new({
+      elements = {
+        'True',
+        'False',
       },
-      typescript = {
-        augend.integer.alias.decimal, -- nonnegative and negative decimal number
-        augend.constant.alias.bool, -- boolean value (true <-> false)
-        logical_alias,
-        augend.constant.new({ elements = { 'let', 'const' } }),
-        ordinalNumbers,
-        weekdays,
-        months,
-      },
-      css = {
-        augend.integer.alias.decimal, -- nonnegative and negative decimal number
-        augend.hexcolor.new({
-          case = 'lower',
-        }),
-        augend.hexcolor.new({
-          case = 'upper',
-        }),
-      },
-      markdown = {
-        augend.misc.alias.markdown_header,
-        ordinalNumbers,
-        weekdays,
-        months,
-      },
-      json = {
-        augend.integer.alias.decimal, -- nonnegative and negative decimal number
-        augend.semver.alias.semver, -- versioning (v1.1.2)
-      },
-      lua = {
-        augend.integer.alias.decimal, -- nonnegative and negative decimal number
-        augend.constant.alias.bool, -- boolean value (true <-> false)
-        augend.constant.new({
-          elements = { 'and', 'or' },
-          word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
-          cyclic = true, -- "or" is incremented into "and".
-        }),
-        ordinalNumbers,
-        weekdays,
-        months,
-      },
-      python = {
-        augend.integer.alias.decimal, -- nonnegative and negative decimal number
-        augend.constant.alias.bool, -- boolean value (true <-> false)
-        logical_alias,
-        ordinalNumbers,
-        weekdays,
-        months,
-      },
+      word = true,
+      cyclic = true,
     })
 
-    -- autocmd
-    local set_dial_group = function(lang)
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "n", "<C-a>", require("dial.map").inc_normal(lang), {})
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "v", "<C-a>", require("dial.map").inc_visual(lang), {})
-
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "n", "<C-x>", require("dial.map").dec_normal(lang), {})
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "v", "<C-x>", require("dial.map").dec_visual(lang), {})
-
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "n", "g<C-a>", require("dial.map").inc_gnormal(lang), {})
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "v", "g<C-a>", require("dial.map").inc_gvisual(lang), {})
-
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "n", "g<C-x>", require("dial.map").dec_gnormal(lang), {})
-      -- stylua: ignore
-      vim.api.nvim_buf_set_keymap(0, "v", "g<C-x>", require("dial.map").dec_gvisual(lang), {})
-    end
-
-    local dial_augroup =
-      vim.api.nvim_create_augroup('DialFileType', { clear = true })
-
-    local filetypes = {
-      typescript = {
-        'typescript',
-        'typescriptreact',
-        'javascript',
-        'javascriptreact',
+    return {
+      dials_by_ft = {
+        css = 'css',
+        sass = 'css',
+        scss = 'css',
+        javascript = 'typescript',
+        javascriptreact = 'typescript',
+        typescript = 'typescript',
+        typescriptreact = 'typescript',
+        json = 'json',
+        lua = 'lua',
+        markdown = 'markdown',
+        python = 'python',
       },
-      css = { 'css', 'scss', 'sass' },
-      markdown = { 'markdown' },
-      json = { 'json' },
-      lua = { 'lua' },
-      python = { 'python' },
+      groups = {
+        default = {
+          augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+          augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+          augend.date.alias['%Y/%m/%d'], -- date (2022/02/19, etc.)
+        },
+        typescript = {
+          augend.integer.alias.decimal, -- nonnegative and negative decimal number
+          augend.constant.alias.bool, -- boolean value (true <-> false)
+          logical_alias,
+          augend.constant.new({ elements = { 'let', 'const' } }),
+          ordinal_numbers,
+          weekdays,
+          months,
+        },
+        css = {
+          augend.integer.alias.decimal, -- nonnegative and negative decimal number
+          augend.hexcolor.new({
+            case = 'lower',
+          }),
+          augend.hexcolor.new({
+            case = 'upper',
+          }),
+        },
+        markdown = {
+          augend.misc.alias.markdown_header,
+          ordinal_numbers,
+          weekdays,
+          months,
+        },
+        json = {
+          augend.integer.alias.decimal, -- nonnegative and negative decimal number
+          augend.semver.alias.semver, -- versioning (v1.1.2)
+        },
+        lua = {
+          augend.integer.alias.decimal, -- nonnegative and negative decimal number
+          augend.constant.alias.bool, -- boolean value (true <-> false)
+          augend.constant.new({
+            elements = { 'and', 'or' },
+            word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+            cyclic = true, -- "or" is incremented into "and".
+          }),
+          ordinal_numbers,
+          weekdays,
+          months,
+        },
+        python = {
+          augend.integer.alias.decimal, -- nonnegative and negative decimal number
+          capitalized_boolean,
+          logical_alias,
+          ordinal_numbers,
+          weekdays,
+          months,
+        },
+      },
     }
-
-    for lang, patterns in pairs(filetypes) do
-      vim.api.nvim_create_autocmd('FileType', {
-        group = dial_augroup,
-        pattern = patterns,
-        callback = function()
-          set_dial_group(lang)
-        end,
-      })
-      print(lang, vim.inspect(patterns))
-
-      for pattern in pairs(patterns) do
-        print(vim.bo.filetype == pattern)
-        if vim.bo.filetype == pattern then
-          print('vim.bo.filetype', vim.bo.filetype, 'lang', lang)
-          set_dial_group(lang)
-          return
-        end
-      end
-    end
-    set_dial_group(vim.bo.filetype)
   end,
 }
