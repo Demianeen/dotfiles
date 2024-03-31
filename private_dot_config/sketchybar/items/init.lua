7bebb7633182565d4c05d3df3c777159429eebca
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-03-31 10:47:53 +0100

    feat(sketchybar): add svim item

--- private_dot_config/sketchybar/items/init.lua
+++ private_dot_config/sketchybar/items/init.lua
@@ -2,6 +2,8 @@ require("items.left.spaces.spaces")
 require("items.left.front_app")
 require("items.left.yabai")
 
+require("items.center.svim")
+
 require("items.right.clock")
 require("items.right.battery")
 require("items.right.media")
