0378f915cbd45e760c50f2a1a29dc86bd3964f85
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-05-28 13:15:36 +0100

    feat(nvim): remove lab.nvim quick data from cmp

--- private_dot_config/nvim/lua/plugins/coding/lab.lua
+++ private_dot_config/nvim/lua/plugins/coding/lab.lua
@@ -5,46 +5,46 @@ vim.keymap.set('n', '<leader>cLi', ':LspInfo<CR>', opts('Lsp Info'))
 vim.keymap.set('n', '<leader>cLl', ':LspLog<CR>', opts('Lsp Log'))
 
 return {
-  'nvim-cmp',
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, 1, {
-      name = 'lab.quick_data',
-      keyword_length = 4,
-    })
-  end,
+  -- 'nvim-cmp',
+  -- ---@param opts cmp.ConfigSchema
+  -- opts = function(_, opts)
+  --   table.insert(opts.sources, 1, {
+  --     name = 'lab.quick_data',
+  --     keyword_length = 4,
+  --   })
+  -- end,
+  -- dependencies = {
+  '0x100101/lab.nvim',
+  build = 'cd js && npm ci',
   dependencies = {
-    '0x100101/lab.nvim',
-    build = 'cd js && npm ci',
-    dependencies = {
-      {
-        'folke/which-key.nvim',
-        opts = function(_, _)
-          local wk = require('which-key')
-          wk.register({
-            l = {
-              name = 'Lab',
-            },
-            L = {
-              name = 'Lsp',
-            },
-          }, { prefix = '<leader>c' })
-        end,
-      },
+    {
+      'folke/which-key.nvim',
+      opts = function(_, _)
+        local wk = require('which-key')
+        wk.register({
+          l = {
+            name = 'Lab',
+          },
+          L = {
+            name = 'Lsp',
+          },
+        }, { prefix = '<leader>c' })
+      end,
     },
-    keys = {
-      { '<leader>cls', ':Lab code stop<CR>', desc = 'Stop', silent = true },
-      { '<leader>clr', ':Lab code run<CR>', desc = 'Run', silent = true },
-      { '<leader>clp', ':Lab code panel<CR>', desc = 'Panel', silent = true },
-      { '<leader>clc', ':Lab code config<CR>', desc = 'Config', silent = true },
+  },
+  keys = {
+    { '<leader>cls', ':Lab code stop<CR>', desc = 'Stop', silent = true },
+    { '<leader>clr', ':Lab code run<CR>', desc = 'Run', silent = true },
+    { '<leader>clp', ':Lab code panel<CR>', desc = 'Panel', silent = true },
+    { '<leader>clc', ':Lab code config<CR>', desc = 'Config', silent = true },
+  },
+  opts = {
+    code_runner = {
+      enabled = true,
     },
-    opts = {
-      code_runner = {
-        enabled = true,
-      },
-      quick_data = {
-        enabled = true,
-      },
+    quick_data = {
+      enabled = true,
     },
   },
 }
+-- }
