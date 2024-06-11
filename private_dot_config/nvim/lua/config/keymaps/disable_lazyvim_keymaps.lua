d2e35ba96a22194e1dab413921afff3ee559c944
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-06-11 13:24:53 +0100

    feat(nvim): remove unnecessary removal of H and L keymaps for lazyvim
    They are overwritten by mini.move

--- private_dot_config/nvim/lua/config/keymaps/disable_lazyvim_keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps/disable_lazyvim_keymaps.lua
@@ -3,7 +3,3 @@ vim.api.nvim_del_keymap('n', '<c-/>')
 vim.api.nvim_del_keymap('t', '<c-/>')
 vim.api.nvim_del_keymap('n', '<c-_>')
 vim.api.nvim_del_keymap('t', '<C-_>')
-
--- next/prev buffer
-vim.keymap.del('n', '<S-h>')
-vim.keymap.del('n', '<S-l>')
