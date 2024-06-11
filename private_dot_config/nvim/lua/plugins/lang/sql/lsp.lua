983f1950a0a61409a7ab51b8844878700fe3362b
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-06-11 13:03:42 +0100

    fix(nvim): disable sql lsp formatting for now
    There is an issue with formatting after which sql becomes gibberish
    https://github.com/sqls-server/sqls/issues/153

--- private_dot_config/nvim/lua/plugins/lang/sql/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lang/sql/lsp.lua
@@ -5,6 +5,8 @@ return {
       servers = {
         sqls = {
           on_attach = function(client, bufnr)
+            client.server_capabilities.documentFormattingProvider = false
+            client.server_capabilities.documentRangeFormattingProvider = false
             require('sqls').on_attach(client, bufnr)
           end,
         },
