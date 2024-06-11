d51915797741e4603df86d22a879d9b74e20c1a7
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-06-11 12:57:30 +0100

    feat(nvim): configure lua lsp

--- private_dot_config/nvim/lua/plugins/lang/lua/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lang/lua/lsp.lua
@@ -7,6 +7,20 @@ return {
           settings = {
             Lua = { workspace = { checkThirdParty = true } },
           },
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {
+              'vim',
+              'require',
+            },
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file('', true),
+          },
         },
       },
     },
