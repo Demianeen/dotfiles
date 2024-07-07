ffd0504d9ef4f1de97ffa2bdb58c3bc472973f66
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-07-07 11:49:55 +0100

    fix(nvim): two snippets entry per snippet in cmp

--- private_dot_config/nvim/lua/extras/my/coding/cmp.lua
+++ private_dot_config/nvim/lua/extras/my/coding/cmp.lua
@@ -28,51 +28,6 @@ return {
           and not context.in_syntax_group('Comment')
       end
 
-      -- opts.sorting = {
-      -- 	---@type cmp.ComparatorFunction
-      -- 	comparators = {
-      -- 		-- function(entry1, entry2)
-      -- 		--   local kind1 = entry1:get_kind()
-      -- 		--   local kind2 = entry2:get_kind()
-      -- 		--   local completionItem = require('cmp.types').lsp.CompletionItemKind
-      -- 		--
-      -- 		--   -- prioritize snippets over keywords
-      -- 		--   if
-      -- 		--     completionItem[kind1] == 'Snippet'
-      -- 		--     and completionItem[kind2] == 'Keyword'
-      -- 		--   then
-      -- 		--     return true
-      -- 		--   elseif
-      -- 		--     completionItem[kind1] == 'Keyword'
-      -- 		--     and completionItem[kind2] == 'Snippet'
-      -- 		--   then
-      -- 		--     return false
-      -- 		--   end
-      -- 		-- end,
-      -- 		cmp.config.compare.offset,
-      -- 		cmp.config.compare.exact,
-      -- 		cmp.config.compare.score,
-      -- 		cmp.config.compare.recently_used,
-      --
-      -- 		function(entry1, entry2)
-      -- 			local _, entry1_under = entry1.completion_item.label:find('^_+')
-      -- 			local _, entry2_under = entry2.completion_item.label:find('^_+')
-      --
-      -- 			entry1_under = entry1_under or 0
-      -- 			entry2_under = entry2_under or 0
-      --
-      -- 			if entry1_under > entry2_under then
-      -- 				return false
-      -- 			elseif entry1_under < entry2_under then
-      -- 				return true
-      -- 			end
-      -- 		end,
-      --
-      -- 		cmp.config.compare.kind,
-      -- 		cmp.config.compare.sort_text,
-      -- 	},
-      -- }
-
       opts.sources = cmp.config.sources({
         {
           name = 'nvim_lsp',
@@ -82,8 +37,6 @@ return {
               ~= 'Keyword'
           end,
         },
-        { name = 'nvim_lsp_signature_help' },
-        { name = 'luasnip', keyword_length = 1 },
       }, {
         { name = 'nvim_lua' },
         { name = 'path' },
