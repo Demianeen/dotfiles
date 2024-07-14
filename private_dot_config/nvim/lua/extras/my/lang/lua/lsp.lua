414afc87b5b09c3253812007ba54749dbf2f2d72
Feliche-Demian Netliukh <51330172+Demianeen@users.noreply.github.com>
2024-07-14 11:07:31 +0100

    fix(nvim): undefined globals warning in lua

--- private_dot_config/nvim/lua/extras/my/lang/lua/lsp.lua
+++ private_dot_config/nvim/lua/extras/my/lang/lua/lsp.lua
@@ -5,21 +5,27 @@ return {
       servers = {
         lua_ls = {
           settings = {
-            Lua = { workspace = { checkThirdParty = true } },
-          },
-          runtime = {
-            version = 'LuaJIT',
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {
-              'vim',
-              'require',
+            Lua = {
+              runtime = {
+                version = 'LuaJIT',
+              },
+              diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {
+                  'vim',
+                  'require',
+                },
+              },
+              workspace = {
+                checkThirdParty = true,
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file('', true),
+              },
+              -- Do not send telemetry data containing a randomized but unique identifier
+              telemetry = {
+                enable = false,
+              },
             },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file('', true),
           },
         },
       },
